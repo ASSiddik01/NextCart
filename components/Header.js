220cc6690f438fcee9b43de41bf368c1f2256297
Shama <assiddik001@gmail.com>
2022-07-26 10:08:55 +0600

    add header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,53 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import logo from "../images/logo.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <Image
+              src={logo}
+              width={180}
+              height={60}
+              alt="This is next cart site logo"
+            />
+          </a>
+          <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+            <Link href={"/"}>
+              <a className="mr-5 hover:text-gray-900">Home</a>
+            </Link>
+            <Link href={"/products"}>
+              <a className="mr-5 hover:text-gray-900">Shop</a>
+            </Link>
+            <Link href={"/about"}>
+              <a className="mr-5 hover:text-gray-900">About</a>
+            </Link>
+            <Link href={"/contact"}>
+              <a className="mr-5 hover:text-gray-900">Contact</a>
+            </Link>
+          </nav>
+          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Button
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
