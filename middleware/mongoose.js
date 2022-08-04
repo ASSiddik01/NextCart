9c0e823676e549723bd3a4cfd51ac2dab8432368
Shama <assiddik001@gmail.com>
2022-08-04 11:14:17 +0600

    show all product

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -1,3 +1,17 @@
+// import mongoose from 'mongoose'
+
+// const connectDb = handler => async (req, res) => {
+//     if (!mongoose.connections[0].readyState) {
+//         await mongoose.connect(process.env.MONGO_URI)
+//         return handler(req, res);
+//     } else {
+//         console.log("inito middle ware")
+//         return handler(req, res);
+//     }
+// }
+
+// export default connectDb;
+
 import mongoose from 'mongoose'
 
 const connectDb = handler => async (req, res) => {
--- pages/products.js
+++ pages/products.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
+import Product from "../models/Product"
+// import connectDb from '../middleware/mongoose';
+import mongoose from 'mongoose'
 
-const Products = () => {
+
+const Products = ({ products }) => {
   return (
     <>
       <Head>
@@ -10,31 +14,44 @@ const Products = () => {
       </Head>
       <div className="mt-10 lg:px-16 px-4">
         <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-4">
-          <Link href={"/product/T-shirt"}>
-            <div className="card card-compact bg-orange-50 pt-4 rounded-md lg:max-w-lg shadow-xl cursor-pointer hover:text-primary">
-              <figure>
-                <img
-                  className="lg:max-h-[40vh] rounded  px-4"
-                  src="https://i.ibb.co/Sy2Mnxz/tops.webp"
-                  alt="product"
-                />
-              </figure>
-              <div className="card-body">
-                <p className="text-sm text-primary">T-shirt</p>
-                <h2 className="card-title text-neutral hover:text-secondary transition duration-0 hover:duration-200 leading-none">
-                  Title
-                </h2>
-                <div className="flex justify-center">
-                  <p className="text-sm">$ 200</p>
-                  <p className="text-sm">S, M, L, XL, XXL</p>
+          {products.map(product => {
+            console.log(product)
+            return <div key={product._id}><Link passHref={true} href={`/product/${product._id}`}>
+              <div className="card card-compact bg-orange-50 pt-4 rounded-md lg:max-w-lg shadow-xl cursor-pointer hover:text-primary">
+                <figure>
+                  <img
+                    className="lg:max-h-[40vh] rounded  px-4"
+                    src={product.thumbnail}
+                    alt="product"
+                  />
+                </figure>
+                <div className="card-body">
+                  <p className="text-sm text-primary">{product.category}</p>
+                  <h2 className="card-title text-neutral hover:text-secondary transition duration-0 hover:duration-200 leading-none">
+                    {product.title}
+                  </h2>
+                  <div className="flex justify-center">
+                    <p className="text-sm">$ {product.price}</p>
+                    <p className="text-sm">{product.brand}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link>
+            </Link></div>
+          })}
         </div>
       </div>
     </>
   );
 };
+
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI)
+  }
+  let products = await Product.find()
+  return {
+    props: { products: JSON.parse(JSON.stringify(products)) }, // will be passed to the page component as props
+  }
+}
 
 export default Products;
